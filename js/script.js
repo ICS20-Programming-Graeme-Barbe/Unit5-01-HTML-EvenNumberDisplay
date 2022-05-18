14d44539fbb1419721d70ce2499918057bf526ff
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-05-18 17:46:09 +0000

    index.html

--- js/script.js
+++ js/script.js
@@ -6,15 +6,21 @@
 // This function tells the user the cost of getting into the museum
 'use strict';
 function submit() {
-	//
+	//Declares variables
 	let max = parseInt(document.getElementById('max').value)
 	let counter = parseInt(document.getElementById('min').value)
 	let numbers = ""
 
-	while (counter <= max) {
-		numbers = numbers + counter + "<br>"
-		counter = counter + 1
+	if (max > counter) {
+		//While loop for counter
+		while (counter <= max) {
+			numbers = numbers + counter + "<br>"
+			counter = counter + 1
+		}
+	} else {
+		numbers = "Please make sure your min is smaller then yout max<br>"
 	}
 
+	//Displays answer
 	document.getElementById('numbers').innerHTML = numbers
 }
